13974c86f6d4e85903ce26bf82f543e456064349
DimasWds <dimaswidysaputra41@gmail.com>
2026-02-24 04:17:58 +0700

    ddd

--- src/app/(main)/front-end/page.tsx
+++ src/app/(main)/front-end/page.tsx
@@ -1,10 +1,10 @@
-import FrontEndHeaderComponent from "@/components/headers/FrontEndHeader/FrontEndHeaderComponent";
+// import FrontEndHeaderComponent from "@/components/headers/FrontEndHeader/FrontEndHeaderComponent";
 import FrontEndHeroSection from "@/components/contents/FrontEndHeroSection/FrontEndHeroSection";
 const FrontEndPage = () => {
   console.log("testing");
   return (
     <main>
-      <FrontEndHeaderComponent />
+      {/* <FrontEndHeaderComponent /> */}
       <FrontEndHeroSection />
     </main>
   );
--- src/components/contents/FrontEndHeroSection/FrontEndHeroSection.tsx
+++ src/components/contents/FrontEndHeroSection/FrontEndHeroSection.tsx
@@ -3,7 +3,7 @@
 import style from "./FrontEndHeroSection.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import { Globe, Github, Archive } from "lucide-react";
+import { Globe, Github, Archive, Layout } from "lucide-react";
 import Magnet from "../../ReacbitsComponents/Magnet/Magnet";
 
 const FrontEndHeroSection = () => {
@@ -47,22 +47,29 @@ const FrontEndHeroSection = () => {
   ];
   return (
     <section className="w-full bg-secondary relative z-20 overflow-hidden">
-      <section className="max-width-custom  mx-auto relative z-30">
+      <section className="max-width-custom mx-auto mt-20 relative z-30">
         <div className="w-full h-10 flex items-center justify-end px-4 mb-20">
           <button className="cursor-pointer">
             <span className="inline-flex gap-0.5 font-bold main-font bg-primary px-2.5 py-1 rounded-md">
               <Archive /> <p>Buka diarsip</p>
             </span>
           </button>
+          <button className="cursor-pointer">
+            <span className="inline-flex gap-0.5 font-bold main-font bg-primary px-2.5 py-1 rounded-md">
+              <Layout /> <p>Buka dikartu</p>
+            </span>
+          </button>
         </div>
+        {/* container card */}
+        <div className="w-full h-[5rem] bg-amber-50"></div>
         {/* container-arsip */}
-        <div className="w-full py-40 px-5">
-          <table className="w-full">
+        <div className="w-full py-40 px-5 hidden md:flex">
+          <table className={`${style.myTable} w-full`}>
             <thead className="text-left">
-              <tr>
-                <th>Tahun</th>
+              <tr className="font-accen-first">
+                <th className="pl-1.5">Tahun</th>
                 <th>Judul</th>
-                <th className="w-[50%]">Teknologi</th>
+                <th className="w-[40%]">Teknologi</th>
                 <th className="max-w-40">Link</th>
               </tr>
             </thead>
@@ -70,16 +77,28 @@ const FrontEndHeroSection = () => {
               {cardProjects.map((items) => {
                 return (
                   <tr key={items.title}>
-                    <td>{items.year}</td>
+                    <th className="text-left pl-1.5 main-font">{items.year}</th>
                     <td>{items.title}</td>
                     <td>{items.tecnology}</td>
+                    <td className="flex gap-1">
+                      <Link
+                        target="_blank"
+                        href={items.linkDemo}
+                        className="w-7.5 aspect-square flex justify-center items-center text-blue-700"
+                      >
+                        <Globe />
+                      </Link>
+                      <Link
+                        target="_blank"
+                        href={items.linkRep}
+                        className="w-7.5 aspect-square flex justify-center items-center"
+                      >
+                        <Github />
+                      </Link>
+                    </td>
                   </tr>
                 );
               })}
-              {/* <tr>
-                <td>Siti</td>
-                <td>Developer</td>
-              </tr> */}
             </tbody>
           </table>
         </div>
